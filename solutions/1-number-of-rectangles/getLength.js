996ab980a2380082f379944da9c56c49a3df6fc7
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-03-28 22:43:29 +0800

    rename parameters.

--- solutions/1-number-of-rectangles/getLength.js
+++ solutions/1-number-of-rectangles/getLength.js
@@ -1,6 +1,6 @@
-const getLength = (a, b) => {
-  const [x1, y1] = a
-  const [x2, y2] = b
+const getLength = (point1, point2) => {
+  const [x1, y1] = point1
+  const [x2, y2] = point2
 
   const diffX = x1 - x2
   const diffY = y1 - y2
